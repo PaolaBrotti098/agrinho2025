a6f547aa9399154d68d20eadcae816e6eaba6c19
PaolaBrotti098 <paola.brotti@escola.pr.gov.br>
2025-06-13 11:53:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,67 +1,115 @@
-
 function setup() {
   createCanvas(400, 400);
 }
+
+function draw() {
+  background(220);
+}
 let Xjogador1 = 0;
 let Xjogador2 = 0;
+let gameOver = false;
+let confetes = [];
 
-
-    function draw() {
-    ativaJogo();
-    desenhaJogadores();
-    desenhaLinhaDeChegada();
-    
-               
-
-      
-   if (Xjogador1 > 350){
-    fill("#FA3461");
-   text("Tecnologia venceu!", 20, 200);
-  noLoop();
+function setup() {
+  createCanvas(400, 400);
+  textAlign(CENTER, CENTER);
 }
-    
-  if (Xjogador2 > 350) {
-    fill("#4B34FA");
-   text("Campo venceu!", 50, 200);
-  noLoop();
-}
-    }   
-      function keyReleased() {
-  if (key === 'c') {
-    Xjogador1 += random (25);
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+
+  if (!gameOver) {
+    if (Xjogador1 > 350) {
+      gameOver = true;
+      vencedor = "Tecnologia venceu!";
+      criaConfetes("#FA3461");
+    } else if (Xjogador2 > 350) {
+      gameOver = true;
+      vencedor = "Campo venceu!";
+      criaConfetes("#4B34FA");
+    }
+  } else {
+    fill(0);
+    textSize(36);
+    text(vencedor, width / 2, height / 2);
+    atualizaConfetes();
   }
-  if(key == 'a'){
-  Xjogador2 += random (25);
-    }
-        
-  }
-
-function ativaJogo (){
-  if (focused==true){
-  background("#34FA6A");
-    }
-    else{
-  background("#34C5FA");
-    }
 }
-function desenhaJogadores(){
+
+function keyReleased() {
+  if (!gameOver) {
+    if (key === 'c') {
+      Xjogador1 += random(25);
+    }
+    if (key === 'a') {
+      Xjogador2 += random(25);
+    }
+  }
+}
+
+function ativaJogo() {
+  background(focused ? "#34FA6A" : "#34C5FA");
+}
+
+function desenhaJogadores() {
   textSize(38);
-  text("🛻", Xjogador1 ,100);
-  text("😺", Xjogador2 ,300);
+  text("🛻", Xjogador1, 100);
+  text("😺", Xjogador2, 300);
 }
-function desenhaLinhaDeChegada(){
- let s=10; 
- for(let x=0;x<4;x++) 
- for(let y=0;y<400/s;y++)
-  {fill((x+y)%2==0?0:255);
-  rect(x*s+300,y*s,s,s);} 
-  stroke(20,0,0); 
+
+function desenhaLinhaDeChegada() {
+  let s = 20;
+  let startX = 350;
+  let rows = height / s;
+
+  let invert = frameCount % 30 < 15;
+
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < rows; y++) {
+      let isBlack = ((x + y) % 2 === 0);
+      if (invert) isBlack = !isBlack;
+      fill(isBlack ? 0 : 255);
+      rect(startX + x * s, y * s, s, s);
+    }
+  }
+
+  stroke(255, 0, 0);
   strokeWeight(4);
-  line(s*10+200,0,s*10+200,height);
-    
-      
+  line(startX + 4 * s, 0, startX + 4 * s, height);
 }
 
+function criaConfetes(cor) {
+  for (let i = 0; i < 100; i++) {
+    confetes.push({
+      x: random(width),
+      y: random(-height, 0),
+      size: random(5, 10),
+      speed: random(2, 5),
+      color: cor,
+      angle: random(TWO_PI),
+      angularSpeed: random(-0.1, 0.1),
+    });
+  }
 }
 
-    
+function atualizaConfetes() {
+  noStroke();
+  for (let c of confetes) {
+    push();
+    translate(c.x, c.y);
+    rotate(c.angle);
+    fill(c.color);
+    rect(0, 0, c.size, c.size / 2);
+    pop();
+
+    c.y += c.speed;
+    c.angle += c.angularSpeed;
+
+    if (c.y > height) {
+      c.y = random(-100, 0);
+      c.x = random(width);
+    }
+  }
+}
